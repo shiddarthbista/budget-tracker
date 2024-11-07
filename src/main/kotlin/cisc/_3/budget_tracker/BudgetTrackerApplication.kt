8a02b29b76e055a55e6ef8140d6a9fceb66c87ee
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2024-11-07 15:32:23 -0500

    Initial commit

---
+++ src/main/kotlin/cisc/_3/budget_tracker/BudgetTrackerApplication.kt
@@ -0,0 +1,11 @@
+package cisc._3.budget_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BudgetTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/cisc/_3/budget_tracker/BudgetTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package cisc._3.budget_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
